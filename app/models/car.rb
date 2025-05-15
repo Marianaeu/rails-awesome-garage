e333e8269b753519283978d4d0746646c7704aee
Mariana Pacuraru <pacuraru.mariana@yahoo.com>
2025-05-15 15:25:30 +0100

    Created the pages, reviews and made it functional

---
+++ app/models/car.rb
@@ -0,0 +1,10 @@
+class Car < ApplicationRecord
+  belongs_to :owner
+  has_many :reviews, dependent: :destroy
+  has_many :favourites, dependent: :destroy
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true, length: { is: 4 }
+  validates :fuel, presence: true, inclusion: { in: %w(petrol diesel electric), message: "%{value} is not a valid fuel" }
+    validates :brand, uniqueness: {scope: [:model,:owner_id], message: "A owner can have a brand once" }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "cleaning database"
+Car.destroy_all
+Owner.destroy_all
+
+puts "creating owner"
+Owner.create(nickname: "Tommy")
+Owner.create(nickname: "Jens")
+Owner.create(nickname: "Michael")
+
+puts "creating cars"
+Car.create(brand: "fiat", model: "500", year: 2017, fuel: "petrol", owner_id: 1)
+Car.create(brand: "toyota", model: "Aygo", year: 2016, fuel: "petrol", owner_id: 2)
+Car.create(brand: "mercedes", model: "Gla", year: 2018, fuel: "petrol", owner_id: 3)
+
+puts "finished seeding"
